90ec09e66542a226217f44de0e6e900ef627fe62
Dr.T. <Aohudyakov@gmail.com>
2025-02-13 03:40:13 +0500

    познакомились с параметризацией

--- tests/test_code.py
+++ tests/test_code.py
@@ -19,3 +19,8 @@ def test_reverse_string_numbers(numbers):
 
 def test_reverse_string_letters(letters):
     assert code.reverse_string("hello") == letters
+
+@pytest.mark.parametrize('value, expected', [('123', '321'), ('321', '123'), ('world', 'dlrow'), ('hello', 'olleh')])
+
+def test_reverse_string(value, expected):
+    assert code.reverse_string(value) == expected
